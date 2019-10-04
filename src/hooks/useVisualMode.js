8c0294fd1039d4f0b97dc922601c7619be922f40
Eddie <eddietseng@outlook.com>
2019-10-03 20:30:44 -0400

    Added useVisualMode to transition between views

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace === false) {
+      history.push(newMode)
+      setMode(newMode)
+      return { mode };
+    } else if (replace === true) {
+      history.pop()
+      history.push(newMode)
+      setMode(newMode)
+      return { mode };
+    }
+
+  }
+
+  function back() {
+    history.pop()
+    setMode(history[history.length - 1])
+    return { mode };
+  }
+
+  return { mode, transition, back };
+}
