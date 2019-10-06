de8bd11e5eafa7022f9dc79d1e4c666e9eca521c
Eddie <eddietseng@outlook.com>
2019-10-06 18:27:27 -0400

    moved data logic to custom hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,75 +1,103 @@
-// import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-// const axios = require('axios');
+const axios = require('axios');
 
 
-// export default function useApplicationData(initial) {
-//   const [state, setState] = useState({
-//     day: "Monday",
-//     days: [],
-//     appointments: {},
-//     interviewers: {}
-//   });
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
 
-//   const setDay = day => setState({ ...state, day });
+  const setDay = day => setState({ ...state, day });
 
-//   function bookInterview(id, interview) {
-//     const appointment = {
-//       ...state.appointments[id],
-//       interview: { ...interview }
-//     };
   
-//     const appointments = {
-//       ...state.appointments,
-//       [id]: appointment
-//     };
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
   
-//     setState({...state, appointments})
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
   
-//     axios
-//       .put(`/api/appointments/${id}`, {
-//         interview: interview
-//       })
-//       .then((response) => {
-//         console.log("THIS IS THE AXIOS RESPONSEE FROM BOOKING AN INTERVIEW!!!! ", response)
-//         return response
-//       })
-//       .catch((error) => {
-//         console.log(error)
-//       })
+    setState({...state, appointments})
+
+
   
-//   }
+    return axios
+    .put(`/api/appointments/${id}`, {
+      interview: interview
+    })
+    .then((response) => {
+      return "saved"
+    })
+    .catch((error) => {
+      return "error"
+    })
   
-//   function deleteInterview (id, interview) {
-//     //console.log(id, interview);
+  }
+
+  function deleteInterview (id, interview) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
   
-  
-//     const appointment = {
-//       ...state.appointments[id],
-//       interview: null
-//     };
-  
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
     
-//     const appointments = {
-//       ...state.appointments,
-//       [id]: appointment
-//     };
-    
-    
-    
-//     axios
-//     .delete(`/api/appointments/${id}`, {
-//       // id: id,
-//       // time: "12pm",
-//       //interview: {student:null, interviewer:null}
-//     })
-//     .then((response) => {
-//       setState({...state, appointments})
-//       //console.log(response)
-//       //console.log("state after appointment change", state.appointments)
-//     })
-  
-    
-//   }
+   return axios
+    .delete(`/api/appointments/${id}`, {
+    })
+    .then((response) => {
+      setState({...state, appointments})
+      return "deleted"
+    })
+    .catch((error) => {
+        return "error"
+      })
+  }
 
-// }
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(
+        axios
+        .get("/api/days")
+      ),
+      Promise.resolve(
+        axios
+        .get("/api/appointments")
+      ),
+      Promise.resolve(
+        axios
+        .get("/api/interviewers")
+      ),
+    ]).then((all) => {
+      setState({ ...state, 
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+  
+      });
+  
+    });
+  },[])
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  };
+
+}
+
